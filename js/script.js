744097215422dc3d7f15910be6b57a2f0e2c2875
Dominika Barrett <barrettdomi@gmail.com>
2020-01-23 12:40:43 +0000

    Zbudowanie kodu HTML wszystkich linków

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const titleClickHandler = function(event){
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  
+
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
@@ -30,10 +30,47 @@ const titleClickHandler = function(event){
       console.log(targetArticle);
   /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
-}
+  }
 
-const links = document.querySelectorAll('.titles a');
+    const optArticleSelector ='.post';
+    const optTitleSelector = '.post-title';
+    const optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
+    const generateTitleLinks = function() {
+
+      /*remove contents of titleList*/
+    const titleList = document.querySelector(optTitleListSelector);
+      titleList.innerHTML = '';
+
+      /*for each article*/
+    const articles = document.querySelectorAll(optArticleSelector);
+
+      for(let article of articles) {
+          /*get the article id*/
+     const articleId = article.getAttribute('id');
+
+          /*find the title element*/
+     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+          /*get the title from the title element*/
+     const articleTitle = article.querySelectorAll();
+
+          /*create HTML of the link*/
+     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle +'</span></a></li>';
+          console.log(linkHTML);
+
+          /*insert link into titleList*/
+          titleList.insertAdjacentHTML('beforeend',linkHTML);
+      }
+
+      const links = document.querySelectorAll('.titles a');
+
+      for(let link of links) {
+          link.addEventListener('click', titleClickHandler);
+      }
+
+    }
+
+    generateTitleLinks();
+
+  }
