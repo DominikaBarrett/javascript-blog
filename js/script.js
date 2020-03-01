109c0412aa04412eeafe4f28ac70751b350b1224
Dominika Barrett <barrettdomi@gmail.com>
2020-03-01 22:53:07 +0000

    correct a titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const titleClickHandler = function (event) {
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
-  
+
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
@@ -29,7 +29,7 @@ const titleClickHandler = function (event) {
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.titles a.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
@@ -38,20 +38,17 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
-  const linkAttribute = clickedElement.getAttribute('href');
+  const articleSelektor = clickedElement.getAttribute('href');
 
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
-  const targetArticle = document.querySelector(articleSelector);
-
+  const targetArticle = document.querySelector(articleSelektor);
 
   /* add class 'active' to the correct article */
-  for (let targetArticle of targetArticles) {
-    targetArticle.classList.add('active');
+  targetArticle.classList.add('active');
+};
 
-  }
-}
 
 const links = document.querySelectorAll('.titles a');
 
@@ -59,13 +56,11 @@ for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
 
-{
+
   const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
-
-
-  function generateTitleLinks(customSelector = ''){
+  function generateTitleLinks(customSelector = '') {
   
 
     const titleList = document.querySelector(optTitleListSelector);
@@ -81,68 +76,24 @@ for (let link of links) {
 
       const articleId = article.getAttribute("id")
 
-    /* find the title element  and get the title from the title element */
+      /* find the title element  and get the title from the title element */
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
       
 
       /* create HTML of the link */
-      const linkHTMLData = { id: articleId, title: articleTitle };
-      const linkHTML = articleLink(linkHTMLData);
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
       console.log(linkHTML);
 
       /* insert link into titleList */
-      html = html + linkHTML;
+      titleList.insertAdjacentHTML('beforeend', linkHTML);
+
+      titleList.innerHTML = html;
+      const links = document.querySelectorAll('.titles a');
+      for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+      }
     }
 
-    titleList.innerHTML = html;
-    const links = document.querySelectorAll('.titles a');
-    for (let link of links) {
-      link.addEventListener('click', titleClickHandler);
-    }
+    generateTitleLinks();
   }
-
-  generateTitleLinks();
-}
-
-
-// function generateTags() {
-// /* find all articles */
-//   const articles = document.querySelectorAll(optArticleSelector);
-
-// /* START LOOP: for every article: */
-// for (let article of articles) {
-
-// /* find tags wrapper */
-// const tagList = article.querySelector(optArticleTagsSelector);
-
-// /* make html variable with empty string */
-// let html = '';
-
-// /* get tags from data-tags attribute */
-// const articleTags = article.getAttribute("data-tags");
-
-// /* split tags into array */
-// const articleTagsArray = articleTags.split(' ');
-
-// /* START LOOP: for each tag */
-// for (let tag of articleTagsArray) {
-
-// /* generate HTML of the link */
-// const linkHTMLData = { id: tag, title: tag };
-// const linkHTML = templates.tagLink(linkHTMLData);
-
-// /* add generated code to html variable */
-// html = html + linkHTML + ' ';
-
-// /* END LOOP: for each tag */
-// tagList.innerHTML = html;
-
-//   /* insert HTML of all the links into the tags wrapper */
-
-// /* END LOOP: for every article: */
-//   for (let link of links) {
-//     link.addEventListener('click', titleClickHandler);
-// }
-
-// generateTags()
